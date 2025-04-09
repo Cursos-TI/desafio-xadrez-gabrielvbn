095a399967a25f412956fc26cb57736a9cd97df5
gabrielvbn <gabriellucena_98@hotmail.com>
2025-04-09 19:19:48 +0000

    Desafio Novato Xadrez

---
+++ xadrezdesafio.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+
+int bispo = 1, rainha , torre = 1 ;
+
+while (torre <= 5) {
+    printf ("Torre - para Direita!\n");
+    torre ++ ;
+}
+do {
+    printf ("Bispo - Cima/Diagonal Direita!\n");
+    bispo ++ ;
+} while (bispo <= 5);
+
+for (rainha = 1; rainha <=8; rainha ++){
+    printf ("Rainha - para Esquerda\n");
+}
+
+return 0;
+
+}
